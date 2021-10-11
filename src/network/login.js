6f7f8daf429aba96ae4ab1acb7cc76e4826185d0
mzlin <81279075+mzlin2020@users.noreply.github.com>
2021-10-11 09:49:28 +0800

    Update login.js

--- src/network/login.js
+++ src/network/login.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function login(config) {
   return new Promise((resolve, reject) => {
     const instance = axios.create({
-      baseURL:'api/login',
+      baseURL:'http://47.106.182.193:8000/login',
       timeout: 5000,
     })
 

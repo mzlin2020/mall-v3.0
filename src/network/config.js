0e17971083e523d74fee214ec1f07e3f5962095b
mzlin <81279075+mzlin2020@users.noreply.github.com>
2021-10-11 15:45:36 +0800

    Update config.js

--- src/network/config.js
+++ src/network/config.js
@@ -3,7 +3,7 @@ export let baseURL = ''
 if (process.env.NODE_ENV === 'production') {
   baseURL = '/api'
 } else if (process.env.NODE_ENV === 'development') {
-  baseURL = '/api'
+  baseURL = 'http://47.106.182.193:8000/'
 } else {
   baseURL = '/api'
 }

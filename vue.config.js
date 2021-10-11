c546c145e631a33e8ed8ea7fbe4a38445d69fb19
mzlin <81279075+mzlin2020@users.noreply.github.com>
2021-10-11 15:14:55 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://47.106.182.193:8000',
+        target: 'http://47.106.182.193:8000/',
         pathRewrite: {
           '^/api': ''
         },

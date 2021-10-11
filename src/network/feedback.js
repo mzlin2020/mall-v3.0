5506593bd748ad92ac40239295a2d43e9cb4a73a
mzlin <81279075+mzlin2020@users.noreply.github.com>
2021-10-11 10:16:41 +0800

    Update feedback.js

--- src/network/feedback.js
+++ src/network/feedback.js
@@ -7,7 +7,7 @@ export function sendFeedback (content) {
   return new Promise((resolve, reject) => {
     // 创建实例
     const instance = axios.create({
-      baseURL: "http://47.106.182.193:8000/moment",
+      baseURL: "api/moment",
       timeout: 5000,
     })
 

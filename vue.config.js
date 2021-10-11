98262b44be3251002659c70897a19d66403afef4
mzlin <81279075+mzlin2020@users.noreply.github.com>
2021-10-11 10:12:28 +0800

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://47.106.182.193:8000',
+        pathRewrite: {
+          '^/api': ''
+        },
+        changeOrigin: true
+      }
+    }
+  }
+}

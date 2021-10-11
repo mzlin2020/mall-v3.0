6d52e147cf916eb80d362c4dcaab1d4c3f625d42
mzlin <81279075+mzlin2020@users.noreply.github.com>
2021-10-11 10:13:34 +0800

    Update register.js

--- src/network/register.js
+++ src/network/register.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function register(name, password) {
   axios({
-    url: "http://47.106.182.193:8000/users",
+    url: "api/users",
     method:'POST',
     data:{
       name,
